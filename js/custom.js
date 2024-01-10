75014f964cface68bb4587292edb1acd18ff0f2a
maziqans <59256796+maziqans@users.noreply.github.com>
2024-01-10 12:30:29 +0800

    changing the local date from US -> UK

--- js/custom.js
+++ js/custom.js
@@ -193,7 +193,7 @@ $(function(){
               const dateTime = new Date(data.datetime);
   
               // Format the date and time
-              const formattedDateTime = dateTime.toLocaleString('en-US', {
+              const formattedDateTime = dateTime.toLocaleString('en-UK', {
                   year: 'numeric',
                   month: 'short',
                   day: 'numeric',
